465caeff6d26191d2420b1154147b6a01e0d459b
Regynald Zambrano <regzam@Regynalds-MacBook-Pro.local>
2021-06-17 11:30:52 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '43368fe0f32841e396cac1360ac467d2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ada225cf-5408-484b-9631-6f3f9f9f50ea'
+};
